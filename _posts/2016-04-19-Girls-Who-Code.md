42c63553a122ea594464e142027e2a5fa3c6c843
jingzhaoyang <jingzhaoyang@163.com>
2016-04-19 10:54:21 +0800

    更改创建缩略图脚本

---
+++ _posts/2016-04-19-Girls-Who-Code.md
@@ -0,0 +1,9 @@
+---
+title: 谁在用树莓派编程
+featured: small-images/GirlsWhoCode.jpg
+layout: post
+---
+<p>
+<embed height="415" width="544" quality="high" allowfullscreen="true" type="application/x-shockwave-flash" src="http://static.hdslb.com/miniloader.swf" flashvars="aid=4408492&page=1" pluginspage="http://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash"></embed>
+</p>
+这个视频是来自"Girls Who Code"组织，这是一个非营利组织，它致力于缩小在科技和工程领域的性别差距。在公共组织和私人合作伙伴的支持下,"Girls Who Code"工作教育,激励,使高中女生的技能和资源,在计算领域寻找机会。
--- create_image.sh
+++ create_image.sh
@@ -1,9 +1,45 @@
 source_dir="./assets/images/"
 target_dir="./assets/small-images/"
+target_width=368
+target_height=180
+scale="scale=2;"
+target_ratio=$(echo "${scale}(${target_width}/${target_height})"|bc)
+
 for i in $(cd ./${source_dir}/;ls *)
 do
-    convert -resize 373x183\! ./${source_dir}/$i ./${target_dir}/$i
+    source_width=$(identify ${source_dir}/${i} |awk -F ' '  '{print $3}'|awk -F 'x' '{print $1}')
+    source_height=$(identify ${source_dir}/${i} |awk -F ' '  '{print $3}'|awk -F 'x' '{print $2}')
+    echo ${source_width}
+    source_ratio=$(echo "${scale}(${source_width}/${source_height})"|bc)
+    echo $source_ratio
+    if [[ ${source_ratio} > ${target_ratio} ]]; then
+      convert -resize ${target_height} ${source_dir}/${i} ${target_dir}/${i}
+      tmp_width=$(identify ./${target_dir}/${i} |awk -F ' '  '{print $3}'|awk -F 'x' '{print $1}')
+      pos_x=$(echo "(${tmp_width}-${target_width})/2"|bc)
+      convert -crop ${target_width}x${target_height}+${pos_x}+0 ${target_dir}/${i} ${target_dir}/${i}
+
+    fi
+    if [[ ${source_ratio} < ${target_ratio} ]]; then
+      convert -resize ${target_width} ${source_dir}/${i} ${target_dir}/${i}
+      tmp_height=$(identify ./${target_dir}/${i} |awk -F ' '  '{print $3}'|awk -F 'x' '{print $2}')
+      pos_y=$(echo "(${tmp_height}-${target_height})/2"|bc)
+      convert -crop ${target_width}x${target_height}+0+${pos_y} ${target_dir}/${i} ${target_dir}/${i}
+
+      #statements
+    fi
+    # tmp_width=$(identify ./${target_dir}/${i} |awk -F ' '  '{print $3}'|awk -F 'x' '{print $1}')
+    # tmp_height=$(identify ./${target_dir}/${i} |awk -F ' '  '{print $3}'|awk -F 'x' '{print $2}')
+    # if [[ ${tmp_width} > ${target_width} ]]; then
+    #   pos_x=$(echo "(${tmp_width}-${target_width})/2"|bc)
+    #   convert -crop ${target_width}x${target_height}+${pos_x}+0 ${target_dir}/${i} ${target_dir}/${i}
+    #   #statements
+    # fi
+    # if [[ ${tmp_height} > ${target_height} ]]; then
+    #   pos_y=$(echo "(${tmp_height}-${target_height})/2"|bc)
+    #   convert -crop ${target_width}x${target_height}+0+${pos_y} ${target_dir}/${i} ${target_dir}/${i}
+    #   # statements
+    # fi
     # composite -gravity southeast -geometry 42x42+290+8  ./weixin-new.png ./${target_dir}/$i ./${target_dir}/$i
     mogrify -font /home/jing/.local/share/fonts/Adobe-Heiti-Std-R.TTF -pointsize 24 -fill white\
-     -weight bolder -gravity southeast -annotate +1-15 @"logo.txt" ./${target_dir}/$i ./${target_dir}/$i
+     -weight bolder -gravity southeast -annotate +1-15 @"logo.txt" ./${target_dir}/${i} ./${target_dir}/${i}
 done
